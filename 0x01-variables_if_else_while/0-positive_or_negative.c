fb5179b1e95ef5078d6050fae42e8145e4177c7f
yaminMbasheer <yaammmiin@gmail.com>
2023-08-14 16:30:22 -0700

    testing another push

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,1 +1,26 @@
-tisesrds
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * print nuber if is positve or negative
+ */
+int main(void)
+{
+	int j;
+	srand(time(0));
+	n=rand()-RAND_MAX*.5;
+	IF (J>0)
+	{
+		printf("%d positive  \n",j);
+	}
+	else if (j==0)
+ {
+                printf("%d  zero \n",j);
+        }
+	else (j<0)
+ {
+                printf("%d nagetive  \n",j);
+        }
+
+	retrun(0)
+}
