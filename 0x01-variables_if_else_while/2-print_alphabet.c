83d8dc4caca3a525955cb3b7950d56406c32a45a
yaminMbasheer <yaammmiin@gmail.com>
2023-08-20 12:49:36 -0700

    ts2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,9 +5,9 @@
  */
 int main(void)
 {
-char alphabet[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char alphabet[26] = "abcdefghijklmnopqrstuvwxyz";
 int i;
-for (i = 0; i < 52; i++)
+for (i = 0; i < 26; i++)
 {
 putchar(alphabet[i]);
 }
