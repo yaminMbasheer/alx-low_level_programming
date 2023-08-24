7430bc0fe140315bd08e0ecc217c101bdbb53c58
yaminMbasheer <yaammmiin@gmail.com>
2023-08-24 00:29:37 -0700

    test 2

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * string_toupper - change all lowercase to uppercase
- * @n: pointer
- * Return: n
- */
+* string_toupper - change all lowercase to uppercase
+* @n: pointer
+* Return: n
+*/
 char *string_toupper(char *n)
-{	
+{
 int i;
 i = 0;
 while (n[i] != '\0')
